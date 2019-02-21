9b6d810641a3291a3662c203819db758b4c14487
Ahmad Shraby <ahmadelshraby10@outlook.com>
2019-02-21 22:36:09 +0200

    Start
    Create GoalsVC
    Build UI

--- goal-list/Controller/GoalsVC.swift
+++ goal-list/Controller/GoalsVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GoalsVC.swift
 //  goal-list
 //
 //  Created by sHiKoOo on 2/21/19.
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoalsVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func addGoalBtn(_ sender: UIButton) {
+    }
+    
 }
 
