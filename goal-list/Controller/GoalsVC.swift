b2406e08fc39b8e973cb0fe88cc9827d72997683
Ahmad Shraby <ahmadelshraby10@outlook.com>
2019-02-21 23:48:30 +0200

    Model
    CoreData Model Entity

--- goal-list/Controller/GoalsVC.swift
+++ goal-list/Controller/GoalsVC.swift
@@ -7,13 +7,16 @@
 //
 
 import UIKit
+import CoreData
 
 class GoalsVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let goal = Goal()    // instance of core data Model
+        
     }
 
     @IBAction func addGoalBtn(_ sender: UIButton) {
